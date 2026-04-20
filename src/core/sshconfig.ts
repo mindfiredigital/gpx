3596ceaf5ccfe75fb762473ade0c0e82adb50204
Ansuman Panda <2004.ansuman@gmail.com>
2026-04-20 18:45:49 +0530

    feat: add update + remove ssh blocks per profile

--- src/core/sshconfig.ts
+++ src/core/sshconfig.ts
@@ -1,9 +1,10 @@
 import fs from 'node:fs';
-import { SSH_DIR, SSH_CONFIG_PATH } from '../lib/constants';
+import { SSH_DIR, SSH_CONFIG_PATH, ExitCode } from '../lib/constants';
 import { backupFile } from './profileManagement/storeBackup';
 import { atomicWrite } from './profileManagement/atomicWrite';
 import { withLock } from './profileManagement/fileLocking';
 import type { Profile } from '../lib/types/Profile.type';
+import { ProfileError } from './profileManagement/errorClass';
 
 const configRegex = (start: string, end: string) => {
   return new RegExp(`${start}[\\s\\S]*?${end}`, 'g');
@@ -60,13 +61,49 @@ const sshKeyPermissionsOk = (keyPath: string): boolean => {
   }
 };
 
+const updateSshConfigForProfile = async (profile: Profile): Promise<void> => {
+  if (!profile.ssh_key) return;
+  if (!sshKeyExists(profile.ssh_key))
+    throw new ProfileError('ssh key not found', ExitCode.SSH_KEY_MISSING);
+  if (!sshKeyPermissionsOk(profile.ssh_key))
+    throw new ProfileError('permission required', ExitCode.PERMISSION_ERROR);
+
+  const config = readSshConfig();
+  const block = buildSshBlock(profile);
+
+  const start = `# BEGIN gpx:${profile.name}`;
+  const end = `# END gpx:${profile.name}`;
+
+  const regex = configRegex(start, end);
+
+  let updated: string;
+
+  if (config.match(regex)) {
+    updated = config.replace(regex, block);
+  } else updated = config.trim() + `\n\n` + block + `\n`;
+
+  await writeSshConfig(updated);
+};
+
+const removeSshConfigForProfile = async (profileName: string): Promise<void> => {
+  const config = readSshConfig();
+
+  const start = `# BEGIN gpx:${profileName}`;
+  const end = `# END gpx:${profileName}`;
+
+  const regex = configRegex(start, end);
+
+  const updated: string = config.replace(regex, '').trim() + '\n';
+
+  await writeSshConfig(updated);
+};
+
 export {
   ensureSshExists,
   ensureSshConfigExists,
   buildSshBlock,
   sshKeyExists,
   sshKeyPermissionsOk,
-  configRegex,
-  readSshConfig,
-  writeSshConfig,
+  updateSshConfigForProfile,
+  removeSshConfigForProfile,
 };
