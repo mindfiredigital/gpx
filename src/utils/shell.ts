9dea76e8749037a20caf1a23b4c784ce21f74a42
Ansuman Panda <ansumanp@mindfiresolutions.com>
2026-04-23 18:46:09 +0530

    fix: update shell.ts - prompt_badge(active_profile)

--- src/utils/shell.ts
+++ src/utils/shell.ts
@@ -1,27 +1,21 @@
 /* eslint-disable no-useless-escape */
+
 const buildBashInitScript = (): string => {
-  return `
-    get_current_profile_name() {
-        local line
-        line="$(gpx current | head -n 1)"
-        if [[ "\$line" == *"Active profile: "* ]]; then
-            GPX_PROMPT_BADGE="[\${line#Active profile: }]"
-        else
-            GPX_PROMPT_BADGE=""
-        fi
-    }
-
-    if [ -z "\$PROMPT_COMMAND" ]; then
-        PROMPT_COMMAND="get_current_profile_name"
-    else 
-        if [[ ! "\$PROMPT_COMMAND" == *"get_current_profile_name"* ]]; then
-            PROMPT_COMMAND="\$PROMPT_COMMAND;get_current_profile_name;"
-        fi
+  return `get_current_profile_name() {
+    local line
+    line="$(gpx current 2>/dev/null | head -n 1)"
+    if [[ "$line" == *"Active profile: "* ]]; then
+        ACTIVE_PROFILE=" [\${line#Active profile: }]"
+    else
+        ACTIVE_PROFILE=""
     fi
+}
 
-    if [[ ! "\$PS1" == *'\${GPX_PROMPT_BADGE}'* ]]; then
-        PS1="\$PS1"' \${GPX_PROMPT_BADGE}'
-    fi`;
+PS1='\\]\\\[\\033]0;$TITLEPREFIX:$PWD\\007\\\]\\\n\\\[\\033[32m\\\]\\u@\\h \\\[\\033[35m\\\]$MSYSTEM \\\[\\033[33m\\\]\\\w\\\[\\033[36m\\\]\`__git_ps1\`\\\[\\033[31m\\\]$ACTIVE_PROFILE\\\[\\033[0m\\\]\\n$ '
+
+if [[ ! "$PROMPT_COMMAND" == *"get_current_profile_name"* ]]; then
+    PROMPT_COMMAND="\${PROMPT_COMMAND}get_current_profile_name;"
+fi`;
 };
 
 export { buildBashInitScript };
