e94f0e07dc857338eefba1e4b428d1208330fb39
ansumanmindfire <ansumanp@mindfiresolutions.com>
2026-05-18 03:54:38 +0530

    feat: update index.tsx for homePage

--- gpx-docs/src/pages/index.tsx
+++ gpx-docs/src/pages/index.tsx
@@ -1,37 +1,18 @@
 import type { ReactNode } from 'react';
-import clsx from 'clsx';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
-import Heading from '@theme/Heading';
-
-import styles from './index.module.css';
-
-function HomepageHeader() {
-  const { siteConfig } = useDocusaurusContext();
-  return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <Heading as="h1" className="hero__title">
-          {siteConfig.title}
-        </Heading>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-      </div>
-    </header>
-  );
-}
+import { HeroSection } from '../components/HomePage/HeroSection'
+import { FeaturesSection } from '../components/HomePage/FeaturesSection';
+import { CTASection } from '../components/HomePage/CTASection';
 
 export default function Home(): ReactNode {
-  const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
-      <HomepageHeader />
-      <main>
-        <HomepageFeatures />
-      </main>
+      title="Home"
+      description="Git Profile eXchanger - switch between git identities instantly"
+    >
+      <HeroSection />
+      <FeaturesSection />
+      <CTASection />
     </Layout>
   );
 }
