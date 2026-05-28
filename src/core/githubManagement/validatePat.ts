fd655744338ef8d5994f3ac39ea85f7eacbe9271
ansumanmindfire <ansumanp@mindfiresolutions.com>
2026-05-28 14:29:07 +0530

    fix: update validatePat

--- src/core/githubManagement/validatePat.ts
+++ src/core/githubManagement/validatePat.ts
@@ -1,7 +1,7 @@
-import { API_BASE } from "../../lib/constants";
-import type { PatValidationResult, GitHubUser, GitHubEmail } from "../../lib/types/ValidatePat.type";
+import { API_BASE } from '../../lib/constants';
+import type { GitHubUser } from '../../lib/types/ValidatePat.type';
 
-export const validatePat = async (pat: string): Promise<PatValidationResult> => {
+export const validatePat = async (pat: string): Promise<GitHubUser> => {
   const headers = {
     Authorization: `Bearer ${pat}`,
     Accept: 'application/vnd.github+json',
@@ -25,18 +25,7 @@ export const validatePat = async (pat: string): Promise<PatValidationResult> => 
     throw new Error('Could not retrieve account information from GitHub.');
   }
 
-  const emailsResponse = await fetch('https://api.github.com/user/emails', { headers });
-
-  let primaryEmail = '';
-  if (emailsResponse.ok) {
-    const emails = (await emailsResponse.json()) as GitHubEmail[];
-    const primary = emails.find((e) => e.primary && e.verified);
-    primaryEmail = primary?.email ?? emails[0]?.email ?? 'no email received';
-  }
-
   return {
     login: user.login,
-    display_name: user.name ?? user.login,
-    email: primaryEmail,
   };
 };
