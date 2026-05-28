b3e6a7c0a59b7e85bfc56e00cfe3d3b9b1f6e3f5
ansumanmindfire <ansumanp@mindfiresolutions.com>
2026-05-28 14:29:07 +0530

    fix: update validatePat

--- src/lib/types/ValidatePat.type.ts
+++ src/lib/types/ValidatePat.type.ts
@@ -1,18 +1,3 @@
-interface GitHubUser {
-    login: string;
-    name: string | null;
+export interface GitHubUser {
+  login: string;
 }
-
-interface GitHubEmail {
-    email: string;
-    primary: boolean;
-    verified: boolean;
-}
-
-interface PatValidationResult {
-    login: string;
-    display_name: string;
-    email: string;
-}
-
-export type { GitHubUser, GitHubEmail, PatValidationResult };
